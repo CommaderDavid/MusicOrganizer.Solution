9b418b6ff38a09e450df2d3a2dfd2c42415afb8d
David Boedigheimer <stealth259@gmail.com>
2020-10-08 15:02:28 -0700

    Initial creation of music organizer.

---
+++ MusicOrganizer/Controllers/ArtistsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using MusicOrganizer.Models;
+
+namespace MusicOrganizer.Controllers
+{
+    public class ArtistsController : Controller
+    {
+
+    }
+}
