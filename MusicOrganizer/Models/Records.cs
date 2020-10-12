c1806a31fec96dd1fee6568870467669fd7a3420
David Boedigheimer <stealth259@gmail.com>
2020-10-12 16:04:48 -0700

    Add start to record lists showing.

--- MusicOrganizer/Models/Records.cs
+++ MusicOrganizer/Models/Records.cs
@@ -8,9 +8,26 @@ namespace MusicOrganizer.Models
         public int Id { get; }
         private static List<Record> _instances = new List<Record> ();
 
-        public Record ()
+        public Record (string title)
         {
-            
+            Title = title;
+            _instances.Add (this);
+            Id = _instances.Count;
+        }
+
+        public static List<Record> GetAll () 
+        {
+            return _instances;
+        }
+
+        public static void ClearAll () 
+        {
+            _instances.Clear ();
+        }
+
+        public static Record Find(int searchId)
+        {
+            return _instances[searchId - 1];
         }
     }
 }
